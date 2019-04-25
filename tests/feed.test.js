8624dcfa41ca325d1dd8a5e5292bc46c9bec0898
kyled526 <koyoldc@vt.edu>
2019-04-25 15:45:03 -0400

    added null checker into the setPic function

--- tests/feed.test.js
+++ tests/feed.test.js
@@ -1,7 +1,13 @@
 const feed = require('../js/feed.js')
 
-test('setpic test', () => {
+test('setpic test with a valid pic', () => {
     
     expect(feed.setPic("https://vignette.wikia.nocookie.net/p__/images/f/f5/EdEEnE.png/revision/latest?cb=20130924162121&path-prefix=protagonist")).toEqual(2);
+
+});
+
+test('setpic test using null', () => {
+    
+    expect(feed.setPic(null)).toEqual(1);
 
 });
